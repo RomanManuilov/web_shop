c7737c5a9ddcdb7dabb20d6312c858a0962f5ce1
Roman <m1r@inbox.ru>
2018-06-12 17:42:17 +0300

    Worked with function cookie in cart.js

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-    $('.page_btn__link').click(function () {
+   let idEvent = $('.page_btn__link').click(function () {
         let name = $(this).attr('id');
-        /*$.cookie(name, name, {
+        $.cookie(name, name, {
             path: '/'
-        });*/
-        $.cookie('5b1f780baa176618e4b175d1', null);
-       /* let test = $.cookie(name);
-        console.log(test);
-        $('.cart_container').attr("id", '1');*/
+        });
     });
+
+    console.log(idEvent);
+    /*let test = $.cookie();
+    console.log(test);
+    $('div[class=cart_container]').attr("id", test);*/
 
 });
