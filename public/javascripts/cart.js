d10eac90e6cb38727e28e95b5571d9d7f3be613a
Roman <m1r@inbox.ru>
2018-06-20 18:10:16 +0300

    work with cookie and obj.json in cart.js

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,32 +1,51 @@
-$(document).ready(function() {
-    let basket = "basket"
-   $('.page_btn__link').click(function () {
-          let idElem = $(this).attr('id');
-          let nameGood = $(this).parents().find('.page_name').text();
-          let priceGood = $(this).parents().find('.page_price').text();
-          let imgGood = $(this).parents().find('.page_block__img').attr('src');
-          console.log("Путь картнки " + imgGood);
-          obj = {
-              name : nameGood,
-              price : priceGood,
-              img : imgGood
-          }
-          let objGoodItem = JSON.stringify(obj);
-
-            $.cookie(basket, objGoodItem, {
-                path: '/'
-            });
+$(document).ready(function () {
+    let basket = "basket";
+    $('.page_btn__link').click(function () {
+        let idElem = $(this).attr('id');
+        let nameGood = $(this).parents().find('.page_name').text();
+        let priceGood = $(this).parents().find('.page_price').text();
+        let imgGood = $(this).parents().find('.page_block__img').attr('src');
+        obj = {
+            name: nameGood,
+            price: priceGood,
+            img: imgGood
+        };
+        let objGoodItem = JSON.stringify(obj);
+        createCookie(objGoodItem);
     });
-    let test = JSON.parse($.cookie(basket));
 
-    for(let item in test){
-;        console.log(test[item].search(/images/g));
-        if(test[item].search(/images/i) > 0){
-                $('.cart_container').append('<img src="' + test[item] + '" class="cart_img">');
-            } else {
-        $('.cart_container').append('<div class="cart_item">' + test[item] + '</div>');
-        console.log(test[item]);
+    function createCookie(value) {
+        let resultValue = JSON.parse(value);
+        let resultGet = JSON.parse(getCookie());
+        if (resultGet === null) {
+            value = '[' +  value + ']';
+            $.cookie(basket, value, {
+                path: '/',
+            });
+        } else if (resultValue.name === resultGet.name) {
+            console.log('Такой товар есть');
+
+        } else {
+            let currentCookie = JSON.parse($.cookie(basket));
+            currentCookie.push(JSON.parse(value));
+            $.cookie(basket, currentCookie, {
+                path: '/',
+            });
         }
     }
+
+    function getCookie() {
+        return $.cookie(basket)
+    }
+
+    let test = getCookie();
     console.log(test)
+
+    /*for (let item in test) {
+        if (test[item].search(/images/i) > 0) {
+            $('.cart_container').append('<img src="' + test[item] + '" class="cart_img">');
+        } else {
+            $('.cart_container').append('<div class="cart_item">' + test[item] + '</div>');
+        }
+    }*/
 });
