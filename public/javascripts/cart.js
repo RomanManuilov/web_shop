0de1e4d6fe28e6b9e23e9314989b96feeb0681c5
Roman <m1r@inbox.ru>
2018-06-21 17:50:54 +0300

    worked with cookie in cart.js then add fun mess()

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,7 +1,10 @@
+//let mess = require('message.js');
+
 $(document).ready(function () {
     let basket = "basket";
+    // создаем объект по клиек, достем "название","цена","путь картики"
     $('.page_btn__link').click(function () {
-        let idElem = $(this).attr('id');
+        //let idElem = $(this).attr('id');
         let nameGood = $(this).parents().find('.page_name').text();
         let priceGood = $(this).parents().find('.page_price').text();
         let imgGood = $(this).parents().find('.page_block__img').attr('src');
@@ -10,29 +13,31 @@ $(document).ready(function () {
             price: priceGood,
             img: imgGood
         };
-        let objGoodItem = JSON.stringify(obj);
-        console.log("before cookie" + objGoodItem)
-        createCookie(objGoodItem);
+        createCookie(obj);
     });
-
+    //Функция создания cookie и проверки на существующие значения в cookie
     function createCookie(value) {
-        let resultValue = JSON.parse(value);
         let x = getCookie();
-        console.log('cookie' + x);
-
+        // Проверяем есть ли в целом cookie, если нет и значение null, то мы создаем $.cookie()
         if (x == null) {
-            value = '[' +  value + ']';
+            value = JSON.stringify(value);// Приводим наш объект value в строку
+            value = '[' + value + ']';// Делаем массив.
+            //Функция создяния cookie в качестве параметров фунции задается ключь, значение - value - объект который создали ранее.
             $.cookie(basket, value, {
                 path: '/',
             });
         } else {
-            let resultGet = JSON.parse(x);
-            if (resultValue.name === resultGet.name) {
+            let cookieForTest = JSON.parse(getCookie());// приводим значение текущего cookie в массив объектов.
+            let testName = cookieForTest.find(function (obj) { //метсодом find проходим по нашему массиву объектов
+                return obj.name === value.name; // Сравниваем значание по ключу .name объектов в нашем массиве с новым объектом который попал на вход нашей функции.
+            });
+            if (testName !== undefined) { // Если с метода find приходит то такого совподения нет, и в else добовляем новый объект в наш массив,
+                // если есть выводим сообщение "Такой товар есть"
+                mess('Такой товар уже есть!');
                 console.log('Такой товар есть');
-
             } else {
                 let currentCookie = JSON.parse($.cookie(basket));
-                currentCookie.push(JSON.parse(value));
+                currentCookie.push(value);
                 currentCookie = JSON.stringify(currentCookie);
                 $.cookie(basket, currentCookie, {
                     path: '/',
@@ -41,18 +46,16 @@ $(document).ready(function () {
         }
 
     }
-
     function getCookie() {
         return $.cookie(basket)
     }
-
     let test = JSON.parse(getCookie());
-    console.log(test)
-
+    //console.log(test);
     for (let item of test) {
-        console.log(item);
-        $('.cart_container').append('<div class="cart_item">' + item.name + '</div>');
-        $('.cart_container').append('<div class="cart_item">' + item.price + '</div>');
-        $('.cart_container').append('<img src="' + item.img + '" class="cart_img"><br>');
+        $('.cart_container').append('<div class="cart"><div class="cart_item">' + item.name + '</div>' + '<div class="cart_item">' + item.price + '</div>' + '<img src="' + item.img + '" class="cart_img"></div>');
+    }
+    function mess (str) {
+        let message = '<div class="message"><div class="massage_name">' + str + '</div></div>'
+        $('.message_container').css({'position':'fixed','top':'0','min-width':'100%','min-height':'100%','background':'rgba(0,0,0,0.5)','align-items':'center'}).append(message);
     }
 });
---
+++ public/javascripts/message.js
@@ -0,0 +1,5 @@
+ let messages = function(str) {
+        let message = '<div class="message"><div class="massage_name">' + str + '</div></div>'
+        return $('.message_container').append(message);
+}
+//module.exports = messages;
